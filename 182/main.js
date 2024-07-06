a19167ac4178b47958d2655178244d43287957f8
ahmedkoresh500 <ahmedkoresh500@gmail.com>
2024-07-06 22:11:35 +0300

    commit 13

--- 182/main.js
+++ 182/main.js
@@ -27,7 +27,7 @@ function getData(apiLink) {
             // if(this.readyState === 4 && this.status === 200){        // two are the same
                 resolved(JSON.parse(this.responseText));                // [T] of [Text] must be uppercase
             } else {
-                rejected(Error("no data is found"));            // Error constructor  =>> change result
+                rejected( Error("no data is found") );            // Error constructor  =>> change result
             };
         };
     });
@@ -35,26 +35,29 @@ function getData(apiLink) {
 
 getData("https://api.github.com/users/elzerowebschool/repos")
 .then(
-    function(resolvedValue) {                   // [1] [anonymous function]
+    (resolvedValue) => {                   // [1] [anonymous function]
 
         console.log(typeof resolvedValue);      // result = object
-        console.log(resolvedValue);
+        console.log(resolvedValue);             // result = JS object
         console.log(resolvedValue.length);      // result = 23
 
         console.log(`${"#".repeat(30)}\n\n`);
 
         resolvedValue.length = 10;
-        return resolvedValue;               // [a] promise avoid [callback hell] or [pyramid of doom]
-    }                                       // [b] [continuous then] no return = error
+        return resolvedValue;               // [a] promise: avoid [callback hell] or [pyramid of doom]
+    }                                       // [b] [continuous then] no return = syntax error
 ).then(
     (resolvedValue) => {                        // [2] [arrow function]
         console.log(typeof resolvedValue);      // result = object
-        console.log(resolvedValue);
+        console.log(resolvedValue);             // result = JS object
         console.log(resolvedValue.length);      // result = 10
+        console.log(`${"#".repeat(20)}`);
 
         console.log(resolvedValue[0]);
         console.log(resolvedValue[0].name);
-        resolvedValue.forEach( (object) => document.write(`<p> ${object.name} </p>`) );
+        resolvedValue.forEach( function(loapedSingleObject){
+            document.write(`<p> ${loapedSingleObject.name} </p>`) 
+    });
     }
 ).catch(
     function(rejected){                         // [anonymous function] [arrow function]
@@ -62,15 +65,16 @@ getData("https://api.github.com/users/elzerowebschool/repos")
     }
 );
 
+// Error constructor change result
 // [catch] = [rejected] function inside [then]      =>> cancel red error
 // console.log(`${rejectedValue}`);                 =>> data about error
 // console.log(`bad ${rejectedValue}`);             =>> no data about error
 
 
 /*
-console.log(getData("https://api.github.com/users/elzerowebschool/repos"));     // result = [promise] object   =>> in a [pending] state
+console.log(getData("https://api.github.com/users/elzerowebschool/repos")); // result = [promise] object => in a [pending] state
 let promise1 = getData("https://api.github.com/users/elzerowebschool/repos");
-console.log(promise1);                                                          // result = [promise] object   =>> in a [pending] state
+console.log(promise1);                                                      // result = [promise] object => in a [pending] state
 
 promise1.then(
     function(resolvedValue){
@@ -81,20 +85,21 @@ promise1.then(
 
 
 /*
-    [1] callback function                       =>> = arrow function
+    [1] callback function                       = [arrow function]
         () => console.log()
         _  => console.log()
-        =>> no return =>> no semicolon =>> no curelybracket 
+        =>> no return =>> no semicolon =>> no curelybracket                 =>> optional
         =>> no function name =>> no function keyword  =>> no parentheses
         =>> uses arrow
 
     [2] anonymous function
         function(){}
-        =>> function without functionName
+        =>> no functionName
 
     [3] arrow function                          =>> = callback function
         () => {}
-        =>> no return =>> no semicolon =>> no curelybracket 
+        _ => {}
+        =>> no return =>> no semicolon =>> no curelybracket                 =>> optional
         =>> no function name =>> no function keyword  =>> no parentheses
         =>> uses arrow
 */
@@ -105,6 +110,7 @@ promise1.then(
 let Hello = () => {
     console.log("hello");
 };
-
 Hello();
 */
+
+// revision
